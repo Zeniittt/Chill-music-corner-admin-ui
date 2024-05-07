c4099dc5a21506114b9db1280249c5789a56b6e8
Zeniittt <tinnqd1702@gmail.com>
2024-05-07 19:47:22 +0700

    done header and sidebar

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <h1>Chào mừng bạn đến với THL Ngoolxn ♥</h1>
+        </header>
+    );
 }
 
 export default Header;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 function Main() {
-    return <h1>Welcome to THK Music</h1>;
+    const nagative = useNavigate();
+
+    const handleClick = () => {
+        // Chuyển hướng đến đường dẫn /home
+        nagative('/home');
+    };
+
+    return (
+        <div>
+            <h1>Welcome to THK Music</h1>
+            <button onClick={handleClick}>Tu bi không tình yêu!</button>
+        </div>
+    );
 }
 
 export default Main;
